fd0a353c339d3112c0aba2124ce1621765665089
GarDI <garkunov.dmitrii@gmail.com>
2024-12-13 22:59:45 +0300

    My commit 1

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using log4net;
+
+namespace MathApp
+{
+    class Program
+    {
+        private static readonly ILog log = LogManager.GetLogger(typeof(Program));
+
+        static void Main(string[] args)
+        {
+            log4net.Config.XmlConfigurator.Configure();
+
+            var math = new MyMathLibrary.MathOperations();
+            Console.WriteLine("Сложение: " + math.Add(5, 3));
+            Console.WriteLine("Вычитание: " + math.Subtract(5, 3));
+        }
+    }
+}
